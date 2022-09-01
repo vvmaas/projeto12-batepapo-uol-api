ff4c6c6a6c9dd15874df5b0beffefff422b40619
Victor Vermaas <vhvermaas@gmail.com>
2022-09-01 11:20:06 -0300

    participants get/post, no validation yet

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
+import dayjs from 'dayjs'
 dotenv.config()
 
 const server = express()
@@ -15,7 +16,42 @@ mongoClient.connect().then(() => {
     db = mongoClient.db('batepapo-uol')
 })
 
+server.post("/participants", async (req,res) => {
+    const { name } = req.body
+    try {
+        await db.collection('participants').insertOne({
+            name: name,
+            lastStatus: Date.now()
+        })
 
+        await db.collection('messages').insertOne({
+            from: name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: 'status',
+            time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`
+        })
+
+        res.sendStatus(201)
+    } catch (err) {
+
+    }
+})
+
+server.get("/participants", async (req,res) => {
+    try{
+    const participantsRaw = await db.collection("participants").find().toArray()
+    const participants = participantsRaw.map(user => {
+        return {
+            name: user.name, 
+            lastStatus: undefined
+        }
+    })
+    res.send(participants)
+    } catch (err) {
+        res.sendStatus(500)
+    }
+})
 
 
 
